80ec2e443e4193f2e2f7e6390d2020f6d6a4ae31
Moha-O <78355179+Moha-O@users.noreply.github.com>
2024-08-31 20:28:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask, request, jsonify
-import joblib
+import pickle  # Remplace joblib par pickle
 from sklearn.preprocessing import MultiLabelBinarizer
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
 # Charger les modèles
 try:
-    chain_classifier = joblib.load('chain_classifier.pkl')
-    sbert_model = SentenceTransformer('sbert_model')
-  
-   # mlb =joblib.load('mlb.pkl')
+    # Charger le modèle ClassifierChain avec pickle
+    with open('chain_classifier.pkl', 'rb') as file:
+        chain_classifier = pickle.load(file)
+    
+    # Charger le modèle SBERT avec pickle
+    with open('sbert_model.pkl', 'rb') as file:
+        sbert_model = pickle.load(file)
+
+    # Charger le MultiLabelBinarizer avec pickle (si nécessaire)
+    # with open('mlb.pkl', 'rb') as file:
+    #     mlb = pickle.load(file)
+    
     print("Les modèles sont chargés correctement.")
 except Exception as e:
     print(f"Erreur lors du chargement des modèles : {e}")
@@ -22,19 +30,17 @@ def home():
 
 @app.route('/predict/<string:question>')
 def predict(question):
-   
-        
-        # Encoder les phrases avec SBERT
+    # Encoder les phrases avec SBERT
     SBERT_embeddings = sbert_model.encode([question])
-        
-        # Faire des prédictions
+    
+    # Faire des prédictions
     predictions = chain_classifier.predict(SBERT_embeddings)
-        
-      
-       # tags = mlb.inverse_transform(predictions)
-        
-        
-    return jsonify({'predictions': predictions})
+    
+    # Convertir les prédictions en tags (si nécessaire)
+    # tags = mlb.inverse_transform(predictions)
+    
+    return jsonify({'predictions': predictions.tolist()})  # Utiliser .tolist() pour rendre JSON sérialisable
+
 @app.route('/salut_perso/<string:first_name>')
 def salut_toi(first_name):
     return f"Salut {first_name} !"
